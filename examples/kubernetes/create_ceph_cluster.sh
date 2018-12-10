793d807da6bd2918fe1da3894033809de72dec13
maxthwell <1037959324@qq.com>
2018-12-10 10:48:44 +0800

    fit to k8s-v1.12

--- examples/kubernetes/create_ceph_cluster.sh
+++ examples/kubernetes/create_ceph_cluster.sh
@@ -2,11 +2,12 @@
 set -ex
 
 ./create_secrets.sh "$@"
-
 kubectl create \
--f ceph-mds-v1-dp.yaml \
 -f ceph-mon-v1-svc.yaml \
--f ceph-mon-v1-dp.yaml \
+-f ceph-mon-v1-ds.yaml \
 -f ceph-mon-check-v1-dp.yaml \
--f ceph-osd-v1-ds.yaml \
+-f ceph-rbac.yaml \
 --namespace=ceph
+
+#-f ceph-mds-v1-dp.yaml \
+#-f ceph-osd-v1-ds.yaml \
--- examples/kubernetes/delete_ceph_cluster.sh
+++ examples/kubernetes/delete_ceph_cluster.sh
@@ -4,4 +4,4 @@ kubectl delete secret ceph-secret-admin --namespace kube-system
 kubectl get pv | awk '/ceph/ {print $1}' | xargs -I{} kubectl delete pv {}
 kubectl delete storageclass slow
 kubectl delete namespace ceph
-kubectl label nodes --all node-type-
+#kubectl label nodes --all node-type-
